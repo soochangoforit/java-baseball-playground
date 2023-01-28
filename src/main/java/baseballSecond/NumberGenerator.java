4a2bb7efb833de724f76744c95ea66d77cecff5f
soochan <tncksdl05@gmail.com>
2023-01-28 21:41:40 +0900

    [v2] feat : 랜덤 숫자 3개 생성 기능 구현

--- src/main/java/baseballSecond/NumberGenerator.java
+++ src/main/java/baseballSecond/NumberGenerator.java
@@ -1,10 +1,23 @@
 package baseballSecond;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NumberGenerator {
 
   public List<Integer> createRandomNumber() {
-    return null;
+
+    List<Integer> numbers = new ArrayList<>();
+
+    while (numbers.size() < 3) {
+      int number = new Random().nextInt(9) + 1;
+      if (numbers.contains(number)) {
+        continue;
+      }
+      numbers.add(number);
+    }
+
+    return numbers;
   }
 }
